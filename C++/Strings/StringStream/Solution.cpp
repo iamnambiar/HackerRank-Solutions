0be7907509f7bbf5ca84a7b0d7bb5348d2c2ff10
Neeraj C <neeraj.cnambiar@gmail.com>
2020-04-07 09:17:08 +0530

    Added solutions to C++ Practice
    Added solutions to C++/Strings

---
+++ C++/Strings/StringStream/Solution.cpp
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/c-tutorial-stringstream/problem
+
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    vector<int> vect;
+    int num;
+    while(ss>>num) {
+        vect.push_back(num);
+        char ch;
+        ss>>ch;
+    }
+    return vect;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
