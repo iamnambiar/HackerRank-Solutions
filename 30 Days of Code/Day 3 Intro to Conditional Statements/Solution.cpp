144f8db9b172405baf0a104264c3ed551123901d
Neeraj C <neeraj.cnambiar@gmail.com>
2020-04-11 10:12:43 +0530

    30 Days of Code
    Day 3: Intro to Conditional Statements

---
+++ 30 Days of Code/Day 3 Intro to Conditional Statements/Solution.cpp
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/30-conditional-statements/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if((N%2 != 0)||((N%2 == 0)&&((N>=6)&&(N<=20)))){
+        cout<<"Weird"<<endl;
+    }
+    else{
+        cout<<"Not Weird"<<endl;
+    }
+    return 0;
+}
