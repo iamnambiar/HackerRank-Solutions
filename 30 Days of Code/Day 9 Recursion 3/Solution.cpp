ad361dbdb9d6c089c4a9eab73eff3a97e429ab19
Neeraj C <neeraj.cnambiar@gmail.com>
2020-04-17 10:31:42 +0530

    30 Days of Code
    Day 9: Recursion 3

---
+++ 30 Days of Code/Day 9 Recursion 3/Solution.cpp
@@ -0,0 +1,33 @@
+// https://www.hackerrank.com/challenges/30-recursion/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the factorial function below.
+int factorial(int n) {
+    if(n > 1){
+        return n * factorial(n-1);
+    }
+    else{
+        return n;
+    }
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
---
+++ 30 Days of Code/Day 9 Recursion 3/Solution.py
@@ -0,0 +1,27 @@
+# https://www.hackerrank.com/challenges/30-recursion/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n > 1:
+        return n * factorial(n-1)
+    else:
+        return n
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
