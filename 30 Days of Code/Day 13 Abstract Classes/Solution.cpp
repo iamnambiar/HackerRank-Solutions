e8c49bb343841828f8c21e3ff294564c21645752
Neeraj C <neeraj.cnambiar@gmail.com>
2020-04-21 14:49:58 +0530

    30 Days of Code
    Day 13: Abstract Classes

---
+++ 30 Days of Code/Day 13 Abstract Classes/Solution.cpp
@@ -0,0 +1,61 @@
+// hackerrank.com/challenges/30-abstract-classes/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Book{
+    protected:
+        string title;
+        string author;
+    public:
+        Book(string t,string a){
+            title=t;
+            author=a;
+        }
+        virtual void display()=0;
+
+};
+
+// Write your MyBook class here
+class MyBook: public Book{
+    protected:
+        int price;
+    //   Class Constructor
+    //   
+    //   Parameters:
+    //   title - The book's title.
+    //   author - The book's author.
+    //   price - The book's price.
+    //
+    // Write your constructor here
+    public:    
+        MyBook(string t, string a, int p) : Book(t, a){
+            this->price = p;
+        }
+    
+    //   Function Name: display
+    //   Print the title, author, and price in the specified format.
+    //
+    // Write your method here
+        void display(){
+            cout<<"Title: "<<title<<endl;
+            cout<<"Author: "<<author<<endl;
+            cout<<"Price: "<<price<<endl;
+        }
+// End class
+};
+
+int main() {
+    string title,author;
+    int price;
+    getline(cin,title);
+    getline(cin,author);
+    cin>>price;
+    MyBook novel(title,author,price);
+    novel.display();
+    return 0;
+}
