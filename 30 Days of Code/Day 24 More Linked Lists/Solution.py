7702cd125b9c7190f9d38faf99ec160f290c21d1
Neeraj C <neeraj.cnambiar@gmail.com>
2020-05-02 15:13:30 +0530

    30 Days of Code
    Day 24: More Linked Lists

---
+++ 30 Days of Code/Day 24 More Linked Lists/Solution.py
@@ -0,0 +1,47 @@
+# https://www.hackerrank.com/challenges/30-linked-list-deletion/problem
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        #Write your code here
+        if head is None:
+            return head
+        temp = head
+        while temp.next is not None:
+            if temp.data == temp.next.data:
+                refNode = temp.next
+                temp.next = temp.next.next
+                del refNode
+            else:
+                temp = temp.next
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
